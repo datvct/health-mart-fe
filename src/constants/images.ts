2f11811877128245af5ebf1f731250f66c6dbd37
datvct <vancongthanhdata10@gmail.com>
2025-03-03 21:52:41 +0700

    feat: add header screen

---
+++ src/constants/images.ts
@@ -0,0 +1,8 @@
+import IconLoudSpeaker from "../../public/images/icon-loudspeaker.svg"
+import ImageHeader from "../../public/images/image-header.png"
+import ImageLogo from "../../public/images/logo-long-chau.svg"
+export const IMAGES = {
+    ImageHeader,
+    IconLoudSpeaker,
+    ImageLogo,
+}
