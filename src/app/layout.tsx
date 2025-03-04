318831768cdaf394f91f6ab933838d16b8bfc30a
datvct <vancongthanhdata10@gmail.com>
2025-03-04 23:43:34 +0700

    refactor: change logic components footers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import Footer from "../components/footer"
 import "../styles/global.css"
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,8 +1,16 @@
 import IconLoudSpeaker from "../../public/images/icon-loudspeaker.svg"
+import ImageIconChungNhan from "../../public/images/iconchungnhan.png"
+import ImageIconChungNhan2 from "../../public/images/iconchungnhan2.png"
 import ImageHeader from "../../public/images/image-header.png"
 import ImageLogo from "../../public/images/logo-long-chau.svg"
+import ImageMomo from "../../public/images/momo.png"
+import ImageZaloPay from "../../public/images/zalopay.png"
 export const IMAGES = {
     ImageHeader,
     IconLoudSpeaker,
     ImageLogo,
+    ImageMomo,
+    ImageIconChungNhan,
+    ImageIconChungNhan2,
+    ImageZaloPay
 }
