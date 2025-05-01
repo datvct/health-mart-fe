1ba2a19f8bf7cab67f783ab600257ad0c4db485e
baoispro <nguyenlegiabao810@gmail.com>
2025-05-01 15:27:25 +0700

    fix: fix responsive and optimized code of hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,105 +1,76 @@
 import { Carousel } from 'antd';
 import Image from 'next/image';
+import { HEROSECTION } from '../constants/images';
 
 const HeroSection = () => {
   return (
     <section>
       <div>
         <div>
-          <Carousel arrows infinite={true}>
-            <div>
+          <Carousel arrows infinite={true} className='!hidden md:!block lg:!block xl:!block 2xl:!block'>
+            {HEROSECTION.Banner_1_PC.map((bannerSrc, index) => (
               <Image
-                src={
-                  'https://cdn.nhathuoclongchau.com.vn/unsafe/1920x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/Banner_full_width_D_1440x256_1_71d9098f44.png'
-                }
+                key={index}
+                src={bannerSrc}
                 alt="banner"
                 className="object-cover h-[227px] lg:h-[calc(100vw_/_1600_*_284)] xl:h-[284px] w-full"
                 height={227}
                 width={1028}
               />
-            </div>
-            <div>
-              <Image
-                src={
-                  'https://cdn.nhathuoclongchau.com.vn/unsafe/1920x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/310325_Banner_Home_1_bf0a1c0ef6.png'
-                }
-                alt="banner"
-                className="object-cover h-[227px] lg:h-[calc(100vw_/_1600_*_284)] xl:h-[284px] w-full"
-                height={227}
-                width={1028}
-              />
-            </div>
-            <div>
-              <Image
-                src={
-                  'https://cdn.nhathuoclongchau.com.vn/unsafe/1920x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/banner_full_width_1_d3a3a4f965.jpg'
-                }
-                alt="banner"
-                className="object-cover h-[227px] lg:h-[calc(100vw_/_1600_*_284)] xl:h-[284px] w-full"
-                height={227}
-                width={1028}
-              />
-            </div>
+            ))}
+          </Carousel>
+          <Carousel infinite={true} className='!block md:!hidden lg:!hidden xl:!hidden 2xl:!hidden'>
+            {HEROSECTION.Banner_1_MB.map((bannerSrc, index) => (
+                <Image
+                  key={index}
+                  src={bannerSrc}
+                  alt="banner"
+                  className="object-cover h-[calc(100vw_/_375_*_188)] w-full"
+                  height={227}
+                  width={1028}
+                />
+              ))}
           </Carousel>
         </div>
         <div className="mt-5 mx-10">
           <div className="flex gap-5 justify-center">
-            <div className="w-2/3 2xl:w-[805px]">
-              <Carousel arrows infinite={true} autoplay={true}>
-                <div>
+            <div className="w-full md:w-2/3 lg:w-2/3 xl:w-2/3 2xl:w-[805px]">
+              <Carousel arrows infinite={true} autoplay={true} className='!hidden md:!block lg:!block xl:!block 2xl:!block'>
+                {HEROSECTION.Banner_2_PC.map((bannerSrc, index) => (
                   <Image
-                    src={
-                      'https://cdn.nhathuoclongchau.com.vn/unsafe/828x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/H1_Desktop_805x246_da1e92a7c6.png'
-                    }
+                    key={index}
+                    src={bannerSrc}
                     alt="banner"
-                    className='rounded-2xl'
+                    className="rounded-2xl"
                     height={246}
                     width={805}
                   />
-                </div>
-                <div>
+                ))}
+              </Carousel>
+              <Carousel arrows infinite={true} autoplay={true} className='!block md:!hidden lg:!hidden xl:!hidden 2xl:!hidden'>
+                {HEROSECTION.Banner_2_MB.map((bannerSrc, index) => (
                   <Image
-                    src={
-                      'https://cdn.nhathuoclongchau.com.vn/unsafe/828x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/Banner_Web_754f67cafc.jpg'
-                    }
+                    key={index}
+                    src={bannerSrc}
                     alt="banner"
-                    className='rounded-2xl'
+                    className="rounded-2xl"
                     height={246}
                     width={805}
                   />
-                </div>
-                <div>
-                  <Image
-                    src={
-                      'https://cdn.nhathuoclongchau.com.vn/unsafe/828x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/Banner_Web_754f67cafc.jpg'
-                    }
-                    alt="banner"
-                    className='rounded-2xl'
-                    height={246}
-                    width={805}
-                  />
-                </div>
+                ))}
               </Carousel>
             </div>
-            <div className="flex flex-col justify-between">
-              <Image
-                src={
-                  'https://cdn.nhathuoclongchau.com.vn/unsafe/425x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/Banner_Ung_Thu_1_185705d391.jpg'
-                }
-                alt="banner"
-                className='rounded-2xl'
-                height={117}
-                width={399}
-              />
-              <Image
-                src={
-                  'https://cdn.nhathuoclongchau.com.vn/unsafe/425x0/filters:quality(90)/https://cms-prod.s3-sgn09.fptcloud.com/Banner_Ung_Thu_1_185705d391.jpg'
-                }
-                alt="banner"
-                className='rounded-2xl'
-                height={117}
-                width={399}
-              />
+            <div className="hidden md:flex lg:flex xl:flex 2xl:flex flex-col justify-between">
+              {HEROSECTION.Banner_3_PC.map((bannerSrc, index) => (
+                <Image
+                  key={index}
+                  src={bannerSrc}
+                  alt="banner"
+                  className="rounded-2xl"
+                  height={117}
+                  width={399}
+                />
+              ))}
             </div>
           </div>
         </div>
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,29 +1,52 @@
-import BackgroundLogin from "../../public/images/background-login.png"
-import FPTLogoSmall from "../../public/images/FPT-logo-small.png"
-import IconLoudSpeaker from "../../public/images/icon-loudspeaker.svg"
-import SwapIcon from "../../public/images/icon-swap.png"
-import ImageIconChungNhan from "../../public/images/iconchungnhan.png"
-import ImageIconChungNhan2 from "../../public/images/iconchungnhan2.png"
-import ImageHeader from "../../public/images/header/image-header.png"
-import FPTIDLogo from "../../public/images/logo-login-fpt.png"
-import LongChauLogo from "../../public/images/logo-login-long-chau.png"
-import ImageLogo from "../../public/images/logo-long-chau.svg"
-import ImageError from "../../public/images/error-page/error_image.svg"
+import BackgroundLogin from '../../public/images/background-login.png';
+import FPTLogoSmall from '../../public/images/FPT-logo-small.png';
+import IconLoudSpeaker from '../../public/images/icon-loudspeaker.svg';
+import SwapIcon from '../../public/images/icon-swap.png';
+import ImageIconChungNhan from '../../public/images/iconchungnhan.png';
+import ImageIconChungNhan2 from '../../public/images/iconchungnhan2.png';
+import ImageHeader from '../../public/images/header/image-header.png';
+import FPTIDLogo from '../../public/images/logo-login-fpt.png';
+import LongChauLogo from '../../public/images/logo-login-long-chau.png';
+import ImageLogo from '../../public/images/logo-long-chau.svg';
+import ImageError from '../../public/images/error-page/error_image.svg';
 
-import ImageMomo from "../../public/images/momo.png"
-import ImageZaloPay from "../../public/images/zalopay.png"
-export const IMAGES = {
-    ImageHeader,
-    IconLoudSpeaker,
-    ImageLogo,
-    BackgroundLogin,
-    FPTIDLogo,
-    LongChauLogo,
-    FPTLogoSmall,
-    SwapIcon,
-    ImageMomo,
-    ImageIconChungNhan,
-    ImageIconChungNhan2,
-    ImageZaloPay,
-    ImageError
-}
+import ImageMomo from '../../public/images/momo.png';
+import ImageZaloPay from '../../public/images/zalopay.png';
+import Banner_1_1_PC from '../../public/images/hero-section/desktop/banner-1/banner-1-1-PC.webp';
+import Banner_1_2_PC from '../../public/images/hero-section/desktop/banner-1/banner-1-2-PC.webp';
+import Banner_2_1_PC from '../../public/images/hero-section/desktop/banner-2/Banner_web_PC_2_1.webp';
+import Banner_2_2_PC from '../../public/images/hero-section/desktop/banner-2/Banner_Web_PC_2_2.jpg';
+import Banner_2_3_PC from '../../public/images/hero-section/desktop/banner-2/Banner_Web_PC_2_3.webp';
+import Banner_3_1_PC from '../../public/images/hero-section/desktop/banner-3/banner-3-1-PC.webp';
+import Banner_3_2_PC from '../../public/images/hero-section/desktop/banner-3/banner-3-2-PC.webp';
+import Banner_1_1_MB from '../../public/images/hero-section/mobile/banner-1/Banner_1_1_MB.webp';
+import Banner_1_2_MB from '../../public/images/hero-section/mobile/banner-1/Banner_1_2_MB.png';
+import Banner_2_1_MB from '../../public/images/hero-section/mobile/banner-2/Banner_MB_2_1.webp';
+import Banner_2_2_MB from '../../public/images/hero-section/mobile/banner-2/Banner_MB_2_2.webp';
+import Banner_2_3_MB from '../../public/images/hero-section/mobile/banner-2/Banner_MB_2_3.webp';
+
+const IMAGES = {
+  ImageHeader,
+  IconLoudSpeaker,
+  ImageLogo,
+  BackgroundLogin,
+  FPTIDLogo,
+  LongChauLogo,
+  FPTLogoSmall,
+  SwapIcon,
+  ImageMomo,
+  ImageIconChungNhan,
+  ImageIconChungNhan2,
+  ImageZaloPay,
+  ImageError,
+};
+
+const HEROSECTION = {
+  Banner_1_PC: [Banner_1_1_PC, Banner_1_2_PC],
+  Banner_2_PC: [Banner_2_1_PC, Banner_2_2_PC, Banner_2_3_PC],
+  Banner_3_PC: [Banner_3_1_PC, Banner_3_2_PC],
+  Banner_1_MB: [Banner_1_1_MB, Banner_1_2_MB],
+  Banner_2_MB: [Banner_2_1_MB, Banner_2_2_MB, Banner_2_3_MB],
+};
+
+export { IMAGES, HEROSECTION };
